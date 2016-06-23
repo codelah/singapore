16b9ea2e21c0195aa83fce923fd0cf257d08ff5d
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2016-06-23 20:11:11 +0800

    feat(singapore) initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os, sys
+from distutils.core import setup
+from distutils.command.install import install as _install
+from setuptools import setup
+
+
+setup(
+  name = 'singapore',
+  packages = ['singapore'],
+  version = '0.0.1',
+  install_requires=[
+        'xmltodict','requests',
+        'onemap'
+  ],
+  description = "Singapore",
+  author = 'CodeSG',
+  author_email = 'zhchua@gmail.com',
+  url = 'https://github.com/codelah/singapore',
+  keywords = ['data.gov.sg', 'python', 'wrapper', 'psi', 'nea', 'onemap', 'singapore'],
+  classifiers = [],
+)
