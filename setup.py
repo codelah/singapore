6dcab67c8d1c1b6cfb670d14ed81446984cf2903
David Chua <zhchua@gmail.com>
2016-06-24 17:37:00 +0800

    Bump 0.0.1.1

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import setup
 setup(
   name = 'singapore',
   packages = ['singapore'],
-  version = '0.0.1',
+  version = '0.0.1.1',
   install_requires=[
         'xmltodict','requests',
         'onemap', 'nea_api'
   ],
-  description = "Singapore",
+  description = "A unified Python API Wrapper for Data.gov.sg services",
   author = 'CodeSG',
   author_email = 'zhchua@gmail.com',
   url = 'https://github.com/codelah/singapore',
