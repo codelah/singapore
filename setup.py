575abbdd2c4325e8a03a3206813e037af7c578ea
David Chua <zhchua@gmail.com>
2016-06-24 17:10:34 +0800

    Update setup.py for nea_api requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
   version = '0.0.1',
   install_requires=[
         'xmltodict','requests',
-        'onemap'
+        'onemap', 'nea_api'
   ],
   description = "Singapore",
   author = 'CodeSG',
