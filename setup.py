ab6615e7e98d1b231897fb2e00d2d5b44c8a79e9
Ray Chen <ray@half0wl.com>
2017-04-12 20:33:48 +0800

    Formatting/PEP8, unused sys/os imports

--- setup.py
+++ setup.py
@@ -1,22 +1,30 @@
-import os, sys
 from distutils.core import setup
 from distutils.command.install import install as _install
 from setuptools import setup
 
 
 setup(
-  name = 'singapore',
-  packages = ['singapore', 'singapore.onemap', 'singapore.nea_api', 'singapore.datagovsg_api'],
-  version = '0.0.1.1',
-  install_requires=[
-        'xmltodict','requests',
-        'onemap', 'nea_api',
+    name='singapore',
+    packages=[
+        'singapore',
+        'singapore.onemap',
+        'singapore.nea_api',
+        'singapore.datagovsg_api'
+    ],
+    version='0.0.1.1',
+    install_requires=[
+        'xmltodict',
+        'requests',
+        'onemap',
+        'nea_api',
         'datagovsg_api'
-  ],
-  description = "A unified Python API Wrapper for Data.gov.sg services",
-  author = 'CodeSG',
-  author_email = 'zhchua@gmail.com',
-  url = 'https://github.com/codelah/singapore',
-  keywords = ['data.gov.sg', 'python', 'wrapper', 'psi', 'nea', 'onemap', 'singapore'],
-  classifiers = [],
+    ],
+    description='A unified Python API Wrapper for Data.gov.sg services',
+    author='CodeSG',
+    author_email='zhchua@gmail.com',
+    url='https://github.com/codelah/singapore',
+    keywords=[
+        'data.gov.sg', 'python', 'wrapper', 'psi', 'nea', 'onemap', 'singapore'
+    ],
+    classifiers=[],
 )
